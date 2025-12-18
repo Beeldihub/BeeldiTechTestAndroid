6707bf29ffa1627f8368ecd6d44a32af4377d20d
Sagbyy <sbouhdjeur.gagny@gmail.com>
2025-12-19 00:54:15 +0100

    feat(db): add Room database setup for equipment management

--- app/src/main/java/com/example/beelditechtest/MainActivity.kt
+++ app/src/main/java/com/example/beelditechtest/MainActivity.kt
@@ -6,17 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import  androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.example.beelditechtest.ui.theme.BeeldiTechTestTheme
 import com.example.beelditechtest.ui.theme.screenBackground
 
---
+++ app/src/main/java/com/example/beelditechtest/data/db/AppDatabase.kt
@@ -0,0 +1,14 @@
+package com.example.beelditechtest.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.beelditechtest.data.db.dao.EquipmentDao
+import com.example.beelditechtest.data.db.entities.EquipmentEntity
+
+@Database(
+    entities = [EquipmentEntity::class],
+    version = 1
+)
+abstract class AppDataBase:  RoomDatabase() {
+    abstract fun equipmentDao(): EquipmentDao
+}
